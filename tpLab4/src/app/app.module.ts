e552b63a65ebb7f28ffa0e30eac9d08eb3f95ef4
noelianieres <macarenanieres@hotmail.com>
2017-06-03 23:42:05 -0300

    Menu con home, login y productos (sin implementacion), estilos

--- tpLab4/src/app/app.module.ts
+++ tpLab4/src/app/app.module.ts
@@ -2,15 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+//pages
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { MenuComponent } from './menu/menu.component';
+
+import { Routes, RouterModule} from '@angular/router';
+
+//animacion
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+export const appRoutes: Routes = [
+    { 
+      path: 'home',
+      component: HomeComponent
+    },
+    { 
+      path: 'login',
+      component: LoginComponent 
+    },
+    { 
+      path: 'productos', 
+      component: ProductosComponent 
+    },
+    { 
+      path: '**', 
+      redirectTo: '' 
+    }
+    
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ProductosComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export const routingComponents = [HomeComponent, LoginComponent,ProductosComponent];
---
+++ tpLab4/src/app/menu/menu.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router} from '@angular/router';
+@Component({
+  selector: 'app-menu',
+  template: `
+              <ul>
+                 <li class="nav-item"><a class="nav-link" routerLink="home" routerLinkActive="active">Home</a></li>
+                 <li class="nav-item"><a class="nav-link" routerLink="login" routerLinkActive="active">Login</a></li>
+                  <li class="nav-item"><a class="nav-link" routerLink="productos" routerLinkActive="active">Productos</a></li>
+              </ul>              
+  `,
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent implements OnInit {
+
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
